26073a60bd9569bab900538dafbe7d0e7a35be90
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2021-02-23 14:15:46 -0300

    Adding Countdowm module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@ import { CompletedChalenges } from "../components/CompletedChalenges";
 import { Countdown } from "../components/Contdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
-import styles from '../styles/pages/Home.module.css'
+
+import Head from 'next/head';
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>     
+    <div className={styles.container}>   
+      <head>
+        <title>Início | move.it</title>
+      </head>  
       <ExperienceBar />
-      
 
       <section>
         <div>
