50424a6832f2ff62459edf08b33f3d23fa77706a
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2021-02-24 17:02:54 -0300

    Criando o componente ChallengeBox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
+import Head from 'next/head';
+
 import { CompletedChalenges } from "../components/CompletedChalenges";
 import { Countdown } from "../components/Contdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from "../components/ChallengeBox";
 
-import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>   
-      <head>
+      <Head>
         <title>Início | move.it</title>
-      </head>  
+      </Head>  
       <ExperienceBar />
 
       <section>
@@ -20,8 +22,10 @@ export default function Home() {
           <CompletedChalenges />
           <Countdown />
         </div>
-
-        <div></div>
+        
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
